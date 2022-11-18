f86860f7a381211fe5de505d008eff3042e0b97c
Natasha <mrs.solts@mail.ru>
2022-11-19 00:49:06 +0300

    style: удалить лишние отступы в коде

--- scripts/index.js
+++ scripts/index.js
@@ -1,14 +1,11 @@
 const popupElement = document.querySelector('.popup');
 const popupOpenButtonElement = document.querySelector('.profile__edit-button');
 const popupCloseButtonElement = popupElement.querySelector('.popup__button-close');
-
 const formElement = popupElement.querySelector('.popup__form');
 const popupNameInput = popupElement.querySelector('.popup__text_name');
 const popupJobInput = popupElement.querySelector('.popup__text_job');
 const profileName = document.querySelector('.profile__name');
 const profileAbout = document.querySelector('.profile__about');
-
-const popupSaveButtonElement = popupElement.querySelector('.popup__button-submit');
 
 const openPopup = function() {
   popupElement.classList.add('popup_is-opened');
