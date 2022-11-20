06e1f9f2f5e242d83aa57b1cd679ff9b69c34de7
Natasha <mrs.solts@mail.ru>
2022-11-20 15:48:10 +0300

    fix: изменить название класса модификатора

--- scripts/index.js
+++ scripts/index.js
@@ -2,8 +2,8 @@ const popupElement = document.querySelector('.popup');
 const popupOpenButtonElement = document.querySelector('.profile__edit-button');
 const popupCloseButtonElement = popupElement.querySelector('.popup__button-close');
 const formElement = popupElement.querySelector('.popup__form');
-const popupNameInput = popupElement.querySelector('.popup__text_name');
-const popupJobInput = popupElement.querySelector('.popup__text_job');
+const popupNameInput = popupElement.querySelector('.popup__text_type_name');
+const popupJobInput = popupElement.querySelector('.popup__text_type_job');
 const profileName = document.querySelector('.profile__name');
 const profileAbout = document.querySelector('.profile__about');
 
