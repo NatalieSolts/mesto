d295984ff3437bca8833fa74d4456be27e350be5
Наталья <mrs.solts@mail.ru>
2022-12-14 21:03:34 +0300

    feat: настроить валидацию полей и форм в js

--- scripts/validate.js
+++ scripts/validate.js
@@ -1,28 +1,82 @@
-// включение валидации вызовом enableValidation
-// все настройки передаются при вызове
-
 const obj = {
-    formSelector: '.popup__form', //селектор формы
-    inputSelector: '.popup__input', //селектор инпутов внутри этой формы
-    submitButtonSelector: '.popup__button-submit',  //селектор кнопки сабмита этой формы
-    inactiveButtonClass: '.popup__button-submit_disabled', //селектор контейнеров для ошибок этой формы
-    inputErrorClass: '.popup__input_type_error', //класс модификатор для инпутов при возникновении ошибки
-    errorClass: '.popup__error_visible' //класс модификатор для дизэйбла кнопки
-  };
+  formSelector: '.popup__form', //селектор формы
+  inputSelector: '.popup__input', //селектор инпутов внутри этой формы
+  submitButtonSelector: '.popup__button-submit',  //селектор кнопки сабмита этой формы
+  inactiveButtonClass: 'popup__button-submit_disabled', //класс модификатор для дизэйбла кнопки
+  inputErrorClass: 'popup__input_type_error', //класс модификатор для инпутов при возникновении ошибки
+  errorClass: 'popup__error_visible' //селектор контейнеров для ошибок этой формы
+};
 
-  // функция, которая добавит обработчики всем полям формы
-  const setEventListeners = (formElement) => {
+// функция, которая добавляет класс с ошибкой
+const showInputError = (formSelector, inputSelector, errorMessage, obj) => {
+  const { inputErrorClass, errorClass } = obj;
+  const errorElement = formSelector.querySelector(`.${inputSelector.id}-error`);
+  inputSelector.classList.add(inputErrorClass);
+  errorElement.textContent = errorMessage;
+  errorElement.classList.add(errorClass);
+}
 
+// функция, которая скрывает класс с ошибкой
+const hideInputError = (formSelector, inputSelector, obj) => {
+  const { inputErrorClass, errorClass } = obj;
+  const errorElement = formSelector.querySelector(`.${inputSelector.id}-error`);
+  inputSelector.classList.remove(inputErrorClass);
+  errorElement.classList.remove(errorClass);
+  errorElement.textContent = '';
+};
+
+// Проверяем валидность поля
+const isValid = (formSelector, inputSelector, obj) => {
+  if (!inputSelector.validity.valid) {
+    showInputError(formSelector, inputSelector, inputSelector.validationMessage, obj);
+  } else {
+    hideInputError(formSelector, inputSelector, obj);
   }
+};
 
-  const enableValidation = () => {
-    const formList = Array.from(document.querySelectorAll('.popup__form'))
-    console.log(formList);
+// Функция принимает массив полей и проверяет наличие невалидного поля
+hasInvalidInput = (inputList) => {
+  return inputList.some((inputSelector) => {
+    return !inputSelector.validity.valid;
+  });
+};
 
-    formList.forEach((formElement) => {
-        setEventListeners(formElement);
+// Функция, которая меняет состояние кнопки
+const toggleButtonState = (inputList, buttonElement, obj) => {
+  const { inactiveButtonClass } = obj;
+  if (hasInvalidInput(inputList)) {
+    buttonElement.classList.add(inactiveButtonClass);
+  } else {
+    buttonElement.classList.remove(inactiveButtonClass);
+  }
+};
+
+// функция, которая добавит обработчики всем полям формы
+const setEventListeners = (formSelector, obj) => {
+  const { inputSelector, submitButtonSelector, ...restObj } = obj;
+  const inputList = [...formSelector.querySelectorAll(inputSelector)];
+  const buttonElement = formSelector.querySelector(submitButtonSelector);
+  toggleButtonState(inputList, buttonElement, restObj);
+  inputList.forEach((inputSelector) => {
+    inputSelector.addEventListener('input', function () {
+      isValid(formSelector, inputSelector, restObj);
+      toggleButtonState(inputList, buttonElement, restObj);
     });
-  };
+  });
+};
+
+const enableValidation = (obj) => {
+  const { formSelector, ...restObj } = obj;
+  const formList = [...document.querySelectorAll(formSelector)];
+  formList.forEach((formSelector) => {
+    formSelector.addEventListener('submit', function(e) {
+      e.preventDefault();
+    });
+    setEventListeners(formSelector, restObj);
+  });
+};
+
+enableValidation(obj);
 
 
 
