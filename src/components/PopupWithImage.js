228bf713c31429c7b4bb17ddff3bf43e45993833
Наталья <mrs.solts@mail.ru>
2023-01-27 17:57:45 +0300

    refactor: перенести переменные в свойства конструктора класса PopupWithImage

--- src/components/PopupWithImage.js
+++ src/components/PopupWithImage.js
@@ -1,13 +1,16 @@
 import Popup from "./Popup.js";
 
 export default class PopupWithImage extends Popup {
-  open(name, link) {
-    const popupImage = this._popup.querySelector('.popup__cards-image');
-    const popupImageName = this._popup.querySelector('.popup__cards-name');
+  constructor(popupSelector) {
+    super(popupSelector);
+    this._popupImage = this._popup.querySelector('.popup__cards-image');
+    this._popupImageName = this._popup.querySelector('.popup__cards-name');
+  }
 
-    popupImage.src = link; // устанавливаем ссылку
-    popupImage.alt = name; // устанавливаем подпись картинке
-    popupImageName.textContent = name;
+  open(name, link) {
+    this._popupImage.src = link; // устанавливаем ссылку
+    this._popupImage.alt = name; // устанавливаем подпись картинке
+    this._popupImageName.textContent = name;
 
     super.open();
   }
