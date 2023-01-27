bef24da74fac4981e2de025ed2c79d899a121494
Наталья <mrs.solts@mail.ru>
2023-01-27 18:18:40 +0300

    refactor: изменить названия переменных

--- src/pages/index.js
+++ src/pages/index.js
@@ -20,8 +20,8 @@ import {
 const formValidatorAddPlace = new FormValidator(validationConfig, popupAddPlace);
 const formValidatorEditProfile = new FormValidator(validationConfig, popupEditProfile);
 const popupWithImage = new PopupWithImage('.popup_type_increase-img');
-const addPlacePopup = new PopupWithForm('.popup_type_add-place', handleFormSubmitAddPlace);
-const editProfilePopup = new PopupWithForm('.popup_type_edit-profile', handleFormSubmitEditProfile);
+const popupAddCard = new PopupWithForm('.popup_type_add-place', handleFormSubmitAddPlace);
+const popupProfileEdit = new PopupWithForm('.popup_type_edit-profile', handleFormSubmitEditProfile);
 const userInfo = new UserInfo({ userNameSelector: '.profile__name', userJobSelector: '.profile__about'});
 
 // класс Section отвечает за отрисовку элементов на странице
@@ -49,22 +49,22 @@ function handleCardClick(name, link) {
 // открывает попап редактирования профиля
 const handleFormEditProfileOpen = () => {
   const { name, job } = userInfo.getUserInfo();
-  editProfilePopup.setFormValues({ name, job });
+  popupProfileEdit.setFormValues({ name, job });
   formValidatorEditProfile.resetValidation(); //для очистки ошибок и управления кнопкой
-  editProfilePopup.open();
+  popupProfileEdit.open();
 }
 
 // открывает попап добавления карточки
 const handleFormAddPlaceOpen = () => {
   formValidatorAddPlace.resetValidation();
-  addPlacePopup.open();
+  popupAddCard.open();
 }
 
 // Возможность редактирования имени и информации о себе
 function handleFormSubmitEditProfile(evt, values) {
   evt.preventDefault();
   userInfo.setUserInfo(values.name, values.job);
-  editProfilePopup.close();
+  popupProfileEdit.close();
 }
 
 // Возможность добавлять карточки
@@ -72,15 +72,15 @@ function handleFormSubmitAddPlace(evt, dataCard) {
   evt.preventDefault();
   renderCard(dataCard);
   formValidatorAddPlace.resetValidation(); // управляем кнопкой сабмита и очищаем поля формы от ошибок
-  addPlacePopup.close();
+  popupAddCard.close();
 }
 
 cardSection.renderCards();
 formValidatorAddPlace.enableValidation();
 formValidatorEditProfile.enableValidation();
 popupWithImage.setEventListeners();
-addPlacePopup.setEventListeners();
-editProfilePopup.setEventListeners();
+popupAddCard.setEventListeners();
+popupProfileEdit.setEventListeners();
 
 // --- СЛУШАТЕЛИ ---
 popupOpenEditButton.addEventListener('click', handleFormEditProfileOpen);
