afd1d991107df70bbf7e9d3f27a968c0322a6984
Наталья <mrs.solts@mail.ru>
2023-02-12 23:05:26 +0300

    refactor: удалить initialCards; добавить константы - селектор попапа обновления авы и кнопки сабмита

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,32 +1,9 @@
-export const initialCards = [
-  {
-    name: 'Архыз',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-  },
-  {
-    name: 'Челябинская область',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-  },
-  {
-    name: 'Иваново',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-  },
-  {
-    name: 'Камчатка',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-  },
-  {
-    name: 'Холмогорский район',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-  },
-  {
-    name: 'Байкал',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-  },
-];
-
 export const cardListSelector = '.cards__list';
+
 export const popupEditProfile = document.querySelector('.popup_type_edit-profile');
-export const popupOpenEditButton = document.querySelector('.profile__edit-button');
 export const popupAddPlace = document.querySelector('.popup_type_add-place');
+export const popupUpdateAva = document.querySelector('.popup_type_update-avatar');
+
+export const popupOpenEditButton = document.querySelector('.profile__edit-button');
 export const popupAddPlaceButton = document.querySelector('.profile__add-button');
+export const popupUpdateAvatarButton = document.querySelector('.profile__avatar-button');
