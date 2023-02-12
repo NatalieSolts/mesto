1ef1138aeedb03b3d77b714d5888ef12bad6611d
Наталья <mrs.solts@mail.ru>
2023-02-12 23:27:59 +0300

    feat: добавить функцию, которая возвращает ID пользователя; селектор аватара в конструктор класса UserInfo

--- src/components/UserInfo.js
+++ src/components/UserInfo.js
@@ -1,22 +1,31 @@
-// Класс `UserInfo` отвечает за управление отображением информации о пользователе на странице.
+// Класс `UserInfo` отвечает за управление отображением информации
+// о пользователе на странице.
 
 export default class UserInfo {
-  constructor({ userNameSelector, userJobSelector }) {
+  constructor({ userNameSelector, userJobSelector, avatarSelector }) {
     this._userName = document.querySelector(userNameSelector);
     this._userJob = document.querySelector(userJobSelector);
+    this._userAvatar = document.querySelector(avatarSelector);
   }
 
   // возвращает объект с данными пользователя
   getUserInfo() {
     return {
       name: this._userName.textContent,
-      job: this._userJob.textContent
+      job: this._userJob.textContent,
     };
   }
 
+  // возвращает ID пользователя
+  getUserId() {
+    return this._userId;
+  }
+
   // принимает новые данные пользователя и добавляет их на страницу
-  setUserInfo(name, job) {
-    this._userName.textContent = name;
-    this._userJob.textContent = job;
+  setUserInfo(userData) {
+    this._userName.textContent = userData.name;
+    this._userJob.textContent = userData.about;
+    this._userId = userData._id;
+    this._userAvatar.src = userData.avatar;
   }
 }
