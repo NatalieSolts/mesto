eea53fe58afed7714f4744a680b0013de3416d75
Наталья <mrs.solts@mail.ru>
2023-02-12 23:12:50 +0300

    feat: добавить новую функциональность в класс Card - постановка и снятие лайков карточки

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,9 +1,15 @@
 export default class Card {
-  constructor(dataCard, selector, handleCardClick) {
+  constructor(dataCard, userId, selector, handleCardClick, {handleDeleteCard, handleLikeClick}) {
+    this._dataCard = dataCard;
     this._name = dataCard.name;
     this._link = dataCard.link;
+    this._cardId = dataCard._id;
+    this._likes = dataCard.likes;
+    this._userId = userId;
     this._selector = selector;
     this._handleCardClick = handleCardClick;
+    this._handleDeleteCard = handleDeleteCard;
+    this._handleLikeClick = handleLikeClick;
   }
 
   _getTemplate() {
@@ -30,23 +36,48 @@ export default class Card {
 
     this._setEventListeners();
 
+// отображение корзины только на своих карточках:
+    if (this._dataCard.owner._id !== this._userId) {
+      this._card.querySelector('.cards__delete').classList.add('cards__delete_hidden')
+    }
+
+    this._showNumOfLikes();
+    this.checkIfCardIsLiked();
+
     return this._card;
   }
 
-  _likeClick() {
-    this._likeButton.classList.toggle('cards__icon-heart_is-active');
+  // Отображение количества лайков карточки
+  _showNumOfLikes() {
+    this._likeCounter = this._card.querySelector('.cards__number-of-likes');
+    this._likeCounter.textContent = this._likes.length;
+    }
+
+  checkIfCardIsLiked() {
+    if (this._likes.find(user => user._id === this._userId)) {
+      this._likeButton.classList.add('cards__icon-heart_is-active');
+      return true
+    } else {
+      this._likeButton.classList.remove('cards__icon-heart_is-active');
+    }
   }
 
-  _deleteClick() {
+  likesCounter(numOfLikes) {
+    this._likes = numOfLikes;
+    this.checkIfCardIsLiked();
+    this._showNumOfLikes();
+  }
+
+  deleteClick() {
     this._deleteButton.closest('.cards__card').remove();
   }
 
    _setEventListeners = () => {
     this._likeButton.addEventListener('click', () => {
-      this._likeClick();
+      this._handleLikeClick(this._cardId);
     });
     this._deleteButton.addEventListener('click', () => {
-      this._deleteClick();
+      this._handleDeleteCard(this._cardId);
     });
     this._cardImage.addEventListener('click', () => {
       this._handleCardClick(this._name, this._link);
