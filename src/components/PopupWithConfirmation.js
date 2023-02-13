ea666087bd2da0c514c04d12cc612a42a55c2d32
Наталья <mrs.solts@mail.ru>
2023-02-13 12:31:40 +0300

    feat: добавить функции в класс PopupWithConfirmation

--- src/components/PopupWithConfirmation.js
+++ src/components/PopupWithConfirmation.js
@@ -1,6 +1,19 @@
-export default class popupWithConfirmation extends Popup {
+import Popup from "./Popup";
+
+export default class PopupWithConfirmation extends Popup {
   constructor(popupSelector) {
     super(popupSelector);
+  }
 
+  handleFormSubmitConfirmation(submit) {
+    this._handleFormSubmitConfirmation = submit;
+  }
+
+  setEventsListeners() {
+    this._form.addEventListener('submit', (evt) => {
+      evt.preventDefault()
+      this._handleFormSubmitConfirmation()
+    })
+    super.setEventsListeners();
   }
 }
